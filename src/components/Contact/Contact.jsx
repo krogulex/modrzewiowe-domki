85be6e66ae0e56c11cc5aa2e8dd7687876b00101
Maciej Krogulski <maciejkrogulski1@gmail.com>
2023-04-25 13:10:21 +0200

    update contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,24 +1,64 @@
 import css from "./Contact.module.css";
 import { TextField } from "@mui/material";
 
+import icons from "../../icons/symbol-defs.svg";
+
 const Contact = () => {
   return (
-    <div  className={css.Contact}>
-      <h5 id="Contact"></h5>
-      <h2>Kontakt</h2>
-      <address>
-        <p>Modrzewiowe Domki</p>
-        <a href="mailto:modrzewiowe.domki@onet.pl">
-          mail: modrzewiowe.domki@onet.pl
-        </a>
-        <a href="tel:+48694225226">tel: +48 694 225 226</a>
-        <p>Wrzosowa 10 76-034 Sarbinowo woj. zachodniopomorskie</p>
-      </address>
-      <form>
-        <TextField id="name" label="Imię" variant="outlined" />
-        <TextField id="phone" label="Numer telefonu" variant="outlined" />
-        <TextField id="text" label="Zapytanie" variant="outlined" />
-      </form>
+    <div className={css.Contact}>
+      <div>
+        <h5 id="Contact"></h5>
+        <h2>Kontakt</h2>
+        <address className={css.address}>
+          <h4>Modrzewiowe Domki</h4>
+          <ul className={css.addressList}>
+            <li className={css.addressListItem}>
+              <h4 className={css.addressSubtitle}>
+                <svg className={css.icon}>
+                  <use href={`${icons}#icon-envelop`}></use>
+                </svg>
+                Mail
+              </h4>
+              <a href="mailto:modrzewiowe.domki@onet.pl">
+                modrzewiowe.domki@onet.pl
+              </a>
+            </li>
+            <li className={css.addressListItem}>
+              <h4 className={css.addressSubtitle}>
+                <svg className={css.icon}>
+                  <use href={`${icons}#icon-phone`}></use>
+                </svg>
+                Telefon
+              </h4>
+              <a href="tel:+48694225226">+48 694 225 226</a>
+            </li>
+            <li className={css.addressListItem}>
+              <h4 className={css.addressSubtitle}>
+                <svg className={css.icon}>
+                  <use href={`${icons}#icon-location`}></use>
+                </svg>
+                Lokalizacja
+              </h4>
+              Wrzosowa 10, 76-034, Sarbinowo, woj. zachodniopomorskie
+            </li>
+          </ul>
+          <iframe
+          className={css.map}
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d9325.739327391402!2d15.9670371!3d54.2431906!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4700302da9bdde1f%3A0x25e6e63a0f214f2b!2sModrzewiowe%20Domki!5e0!3m2!1spl!2spl!4v1682419126130!5m2!1spl!2spl"
+            style={{border:'0'}}
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </address>
+      </div>
+      <div>
+        <form>
+          <TextField id="name" label="Imię" variant="outlined" />
+          <TextField id="phone" label="Numer telefonu" variant="outlined" />
+          <TextField id="text" label="Zapytanie" variant="outlined" />
+        </form>
+      </div>
     </div>
   );
 };
---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,20 @@
+import { useMemo } from "react";
+import css from "./Map.module.css";
+
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+
+export const Map = () => {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAP_API_KEY,
+  });
+
+  if (!isLoaded) return <div>Map is loading...</div>;
+  return (
+    <GoogleMap
+      zoom={10}
+      center={{ lat: 44, lng: -80 }}
+      mapContainerClassName={css.mapContainer}
+    >
+    </GoogleMap>
+  );
+};
