3f5c33aa05ac5ede378f0807a3dde042d6a77f53
Maciej K <104220768+krogulex@users.noreply.github.com>
2025-05-27 21:08:51 +0000

    zmiana zdjęc vol 1

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -20,18 +20,42 @@ import img7 from "../../images/kuchnia.jpg";
 import img8 from "../../images/łazienka.jpg";
 import img9 from "../../images/pokój-1.jpg";
 import img10 from "../../images/pokój-2.jpg";
+import img11 from "../../images/przód z drzewem.jpg"
+
+import img12 from "../../images/plac zabaw.jpg";
+import img13 from "../../images/kuchnia.jpg";
+import img14 from "../../images/leżaki.jpg";
+import img15 from "../../images/140.jpg";
+import img16 from "../../images/kuchnia.jpg";
+import img17 from "../../images/łóżka.jpg"
+import img18 from "../../images/jadalnia.jpg"
+import img19 from "../../images/plac zabaw 2.jpg";
+import img20 from "../../images/plac zabaw 3.jpg"
+import img21 from "../../images/jadalnia.jpg"
+import img22 from "../../images/143.jpg"
+
+
 
 const images = [
   { src: img1, alt: "domki-przód" },
+ // { src: img11, alt: "domki przód z drzewem" },
+ // { src: img15, alt: "tył" },
   { src: img2, alt: "domki-tył" },
-  { src: img3, alt: "plac-zabaw" },
-  { src: img4, alt: "plac-zabaw" },
-  { src: img5, alt: "domki-tył" },
-  { src: img6, alt: "łazienka" },
-  { src: img7, alt: "salon" },
+    { src: img19, alt: "plac zabaw" },
+      { src: img20, alt: "plac zabaw" },
+    { src: img12, alt: "plac-zabaw" },
+       { src: img22, alt: "plac-zabaw" },
+  //{ src: img3, alt: "plac-zabaw" },
+  //{ src: img4, alt: "plac-zabaw" },
+  { src: img18, alt: "jadalnia" },
+    { src: img13, alt: "jadalnia" },
+  // { src: img14, alt: "leżaki" },
+  //{ src: img5, alt: "domki-tył" },
+ // { src: img6, alt: "łazienka" },,
   { src: img8, alt: "kuchnia" },
-  { src: img9, alt: "pokój-1" },
-  { src: img10, alt: "pokój-2" },
+ // { src: img9, alt: "pokój-1" },
+  { src: img17, alt: "łóżka" },
+ // { src: img10, alt: "pokój-2" },
 ];
 
 const Gallery = () => {
